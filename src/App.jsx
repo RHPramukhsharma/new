63289b2407a7c6ccd95e8f9ef4e8cf22654b3b64
RHpramukh <pramukh@bdsus.net>
2024-05-08 16:22:41 -0400

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  const username ="pramukh"
+  return (
+    <>
+      <h1> new app {username}</h1>
+      <h1> new app {2 + 2}</h1>
+    </>
+  );
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// const areactElement = React.createElement(
+//   "a",
+//   {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   "clicfk me to go visit google"
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
